ab160b8fbf38e13c6ded12a57283d3d8c4bf0c95
AkashMittal-30 <akash.mittal302001@gmail.com>
2022-05-07 15:33:32 +0530

    footer changed

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import '../Styles/Footer.css'
+import logo from '../Images/bluel.png'
 export const Footer = () => {
   return (
     <>
     <footer className='footer-container'>
+      <div className="foot-left">
+        <img src={logo} alt="logo" srcset="" />
+        Re-<span id="medi">Medi</span>
+      </div>
+     <div className="foot-right">
+       <span id="privacy">Privacy policy?</span><br/>
      Copyright &copy; 2022 Re-Medi.  All rights reserved.
+     </div>
     </footer>
     </>
   )
